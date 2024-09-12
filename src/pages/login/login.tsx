6f4d47a3f715918d049d7c914055e80bfe7f546d
Desmond <desmondzkx@gmail.com>
2024-09-12 09:51:56 -0400

    Remove screen testing

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -73,16 +73,6 @@ export default function Page() {
 
     return (
         <div className="flex w-screen h-screen justify-center">
-
-            {/* testing */}
-            <div>
-                <p className="flex">undersmall</p>
-                <p className="hidden sm:block">small bk</p>
-                <p className="hidden md:block">medium bk</p>
-                <p className="hidden lg:block">large bk</p>
-                <p className="hidden xl:block">extra large bk</p>
-                <p className="hidden 2xl:block">extra extra large bk</p>
-            </div>
             <div className="w-2/3 px-5 m-5 justify-center items-center hidden lg:block">
                 <div className="relative w-full h-full">
                     {images.map((image, index) => (
