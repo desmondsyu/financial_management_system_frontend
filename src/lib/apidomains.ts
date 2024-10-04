c6e20aafa03f7ddd048a642b58bd2c3dd7ca250f
Desmond <desmondzkx@gmail.com>
2024-10-04 19:03:05 -0400

    Update apidomains.ts

--- src/lib/apidomains.ts
+++ src/lib/apidomains.ts
@@ -1,2 +1,2 @@
-export const javaApi = "https://recipes.vitalysukhinin.com/fin-api";
+export const javaApi = "https://107.20.240.135/fin-api";
 export const pythonApi = "https://primary-mustang-eagerly.ngrok-free.app";
